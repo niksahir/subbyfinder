985d23d2f610112445e53e35475f0d57f8db93a3
vivek-1445 <vivekvora8485@gmail.com>
2025-05-22 20:04:07 +0530

    make changes

--- routes/web.php
+++ routes/web.php
@@ -126,3 +126,4 @@ Route::prefix('sub-contractor')->name('subcontractor.')->group(function () {
 Route::post('checkout', [StripeController::class, 'checkout'])->name('stripe.checkout');
 Route::get('success', [StripeController::class, 'success'])->name('stripe.success');
 Route::get('cancel', [StripeController::class, 'cancel'])->name('stripe.cancel');
+});
